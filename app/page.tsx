5e6932bfa4b9371e0ec64f8da7051be154097cb4
mandes-mega <mandes-mega@mandes-megadeMacBook-Air.local>
2026-06-08 10:43:56 +0800

    Add missing file for experience showcase section

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import ExperiencesShowcase from "@/components/ExperienceSection";
 import HeroSection from "@/components/HeroSection";
 import MenuShowcase from "@/components/MenuShowcase";
 import Navbar from "@/components/Navbar";
-import LocationSection from "@/components/LocationSection";
 
 export default function HomePage() {
   return (
