28ff33cb54a412c3bb48b114b49ba73d8c1034cb
mandes-mega <mandes@megaannum.ai>
2026-06-15 16:27:25 +0800

    Wire hero and navbar navigation links

--- components/HeroContent.tsx
+++ components/HeroContent.tsx
@@ -2,6 +2,7 @@
 
 import { useTranslation } from "react-i18next";
 import { typography } from "@/lib/typography";
+import Link from "next/link";
 
 export default function HeroContent() {
   const { t } = useTranslation();
@@ -18,18 +19,18 @@ export default function HeroContent() {
         {t("hero.description")}
       </p>
       <div className="mt-7 flex flex-wrap justify-end gap-2.5 sm:mt-[30px] sm:gap-3">
-        <a
-          href="#"
+        <Link
+          href={t("Whatsapp.link")}
           className={`${typography.button} inline-flex h-11 min-w-[140px] items-center justify-center border border-white/60 bg-white/10 px-4 transition-colors duration-200 hover:bg-white/15 sm:h-[45px] sm:min-w-[156px] sm:px-[18px]`}
         >
           {t("hero.reserveTable")}
-        </a>
-        <a
-          href="#"
+        </Link>
+        <Link
+          href="/menu"
           className={`${typography.button} inline-flex h-11 min-w-[140px] items-center justify-center border border-white/60 bg-black/10 px-4 transition-colors duration-200 hover:bg-black/20 sm:h-[45px] sm:min-w-[156px] sm:px-[18px]`}
         >
           {t("hero.viewMenu")}
-        </a>
+        </Link>
       </div>
     </div>
   );
